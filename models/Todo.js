aecc42c21c9f40c5a9e8489b531888cbffad2b4f
pooja <https://github.com/poojatayde9960/ecom.git>
2024-07-03 11:59:56 +0530

    schema updated

--- models/Todo.js
+++ models/Todo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const todoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    // desc: { type: String, required: true },
-    // priority: { type: String, required: true },
+    task: { type: String, required: true },
+    desc: { type: String, required: true },
+    priority: { type: Number, required: true },
     // priority: { type: String, required: true },
     // image: { type: String, required: true },
     // priority: { type: Number, required: true },
