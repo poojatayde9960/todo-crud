6bff08da8e0d93db3fea758d7cea2f76d8ee6d4b
pooja <https://github.com/poojatayde9960/ecom.git>
2024-07-03 12:15:20 +0530

    schema updated

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express()
 app.use(express.json())
 app.use(cors())
+
 app.use("/api/todos", require("./routes/todo.routes"))
 app.use((req, res) => {
     res.status(404).json({ message: "Resource Not Found" })
+})
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: "Server Error" })
 })
 
 mongoose.connection.once("open", () => {
